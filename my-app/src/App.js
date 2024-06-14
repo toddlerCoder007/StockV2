df11ea5ce69168e70399a150b4475c062a596e1e
ChowNUSISS <e0385034@u.nus.edu>
2024-06-14 23:09:46 +0800

    added pic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,29 +2,41 @@
 import './App.css';
 import MyComp from './MyComp';
 import MyExp from './MyExp';
+import pic from './assets/pic1.png';
+
+console.log(pic);
 //import MyComp from './MyComp';
 function App() {
   return (
     <div className="App">
-      <h1> my first app
+      <h1> My first app
         my name is Chow
         <h2> today is 5th May</h2>
         
         <ol> 
         <li>eat rice dumpling</li>
-          <li>Do this</li>
-        <li>do that</li>
+          <li>watch dragon boat</li>
+        <li>pray n reflect</li>
           
         </ol>
         <div>
           <input type="checkbox" />
-          <label> eat Big Mac</label>
+          <label> change money</label>
         </div>
+
+        
         
       </h1>
       <MyComp />
       
       <MyExp />
+
+
+      <h2> Add picture</h2>
+
+      return <img src={pic} alt="Pic" />;
+
+
     </div>
 
 
