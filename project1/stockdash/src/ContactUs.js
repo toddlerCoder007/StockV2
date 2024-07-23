0e619c190fc9a347d5aa18a89f99b69eccecefe8
Tay Li Na <lina.tay96@gmail.com>
2024-07-23 21:28:01 +0800

    prettify contactUs page

--- project1/stockdash/src/ContactUs.js
+++ project1/stockdash/src/ContactUs.js
@@ -1,5 +1,7 @@
-// src/components/ContactUs.js
+// TO-DO: more validation checks
+
 import React, { useState } from "react";
+import "./ContactUs.css";
 
 const ContactUs = () => {
   const [name, setName] = useState("");
@@ -14,48 +16,53 @@ const ContactUs = () => {
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h2>Contact Us</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name: </label>
+    <div className="contact-us-container">
+      <form onSubmit={handleSubmit} className="contact-form">
+        <div className="form-group">
+          <label>Name:</label>
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            className="form-control"
           />
         </div>
-        <div>
-          <label>Email: </label>
+        <div className="form-group">
+          <label>Email:</label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
           />
         </div>
-        <div>
-          <label>Subject: </label>
+        <div className="form-group">
+          <label>Subject:</label>
           <input
             type="text"
             value={subject}
             onChange={(e) => setSubject(e.target.value)}
+            className="form-control"
           />
         </div>
-        <div>
-          <label>Message: </label>
+        <div className="form-group">
+          <label>Message:</label>
           <textarea
             value={message}
             onChange={(e) => setMessage(e.target.value)}
+            className="form-control"
           ></textarea>
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
       </form>
-      <div style={{ marginTop: "20px" }}>
+      <div className="contact-info">
         <p>Email: support@stockportfolio.com</p>
         <p>Phone: (123) 456-7890</p>
         <p>Support Hours: Mon-Fri, 9 AM - 5 PM</p>
       </div>
-      <div style={{ marginTop: "20px" }}>
+      <div className="social-links">
         <h3>Follow Us</h3>
         <a href="https://facebook.com">Facebook</a> |{" "}
         <a href="https://twitter.com">Twitter</a> |{" "}
