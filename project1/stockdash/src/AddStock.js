c3782bc141b1ead0d4599d91518fab41593b6db5
Yichen <53641179+yeyehandsome1984@users.noreply.github.com>
2024-07-26 19:36:27 +0800

    Update AddStock.js

--- project1/stockdash/src/AddStock.js
+++ project1/stockdash/src/AddStock.js
@@ -3,13 +3,15 @@ import React, { useState } from 'react';
 const AddStock = ({ handleAdd }) => {
   const [name, setName] = useState('');
   const [symbol, setSymbol] = useState('');
+  const [quantity, setQuantity] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newStock = { Name: name, Symbol: symbol };
+    const newStock = { Name: name, Symbol: symbol, Quantity: quantity };
     handleAdd(newStock);
     setName('');
     setSymbol('');
+    setQuantity(0);
   };
 
   return (
@@ -21,6 +23,10 @@ const AddStock = ({ handleAdd }) => {
       <div>
         <label>Symbol:</label>
         <input type="text" value={symbol} onChange={(e) => setSymbol(e.target.value)} required />
+      </div>
+      <div>
+        <label>Quantity:</label>
+        <input type="number" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} required />
       </div>
       <button type="submit">Add Stock</button>
     </form>
